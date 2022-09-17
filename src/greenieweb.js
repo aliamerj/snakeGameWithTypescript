73a30351023fff8b14cc97e172750c38b8002d07
Ali Amer <76897266+aliamerj@users.noreply.github.com>
2022-09-17 17:17:27 +0300

    added greenieweb file

---
+++ src/greenieweb.js
@@ -0,0 +1,1 @@
+(function reduceColor() {const imgs = document.body.querySelectorAll("img");for (const img of imgs) {img.style.filter ="contrast(80%) grayscale(80%) opacity(90%) saturate(18%)";}})();
